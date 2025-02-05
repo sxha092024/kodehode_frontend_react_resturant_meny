9423c15d94ac0d7715c746953308836451bc19f8
sxha092024 <183362499+sxha092024@users.noreply.github.com>
2025-02-05 13:56:06 +0100

    chore: format repo

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { } from "./menu_data";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <h1>Vite + React</h1>
   </StrictMode>,
-)
+);
