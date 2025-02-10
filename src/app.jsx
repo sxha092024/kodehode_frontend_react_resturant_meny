ea5f17f9831dd474fd61c24c8d17ecc4b02a274e
sxha092024 <183362499+sxha092024@users.noreply.github.com>
2025-02-10 12:18:15 +0100

    hand off data to menu component

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import MENU from "./menu_data";
+import Menu from "./menu";
+
+import "./styles/app.css";
+
+function App() {
+  return (
+    <>
+      <Menu menu={MENU} />
+    </>
+  );
+}
+
+export default App;
