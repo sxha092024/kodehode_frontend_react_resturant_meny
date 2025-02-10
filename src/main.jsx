c3859729f067f92a3974e7aba3ede03e1c20b946
sxha092024 <183362499+sxha092024@users.noreply.github.com>
2025-02-10 12:18:01 +0100

    Correct imports and use `App` component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { } from "./menu_data";
+import "./styles/index.css";
+import App from "./app";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>Vite + React</h1>
+    <App />
   </StrictMode>,
 );
