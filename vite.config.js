855847e152a7d86385a32bf54e7637e2a27849fb
sxha092024 <183362499+sxha092024@users.noreply.github.com>
2025-02-10 12:26:49 +0100

    remove caching work-around

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames:
-          `[name]` + (Math.floor(Math.random() * 9_000) + 10_000) + `.js`,
-        chunkFileNames:
-          `[name]` + (Math.floor(Math.random() * 9_000) + 10_000) + `.js`,
-        assetFileNames:
-          `[name]` + (Math.floor(Math.random() * 9_000) + 10_000) + `.[ext]`,
-      },
-    },
-  },
 });
